a802b8433741b1f5b95d44a4f69d5c2a9c42dfc6
Delight Bessie <147700339+Bessiedelight@users.noreply.github.com>
2025-04-26 13:18:13 +0000

    required

--- models/patientModel.js
+++ models/patientModel.js
@@ -6,10 +6,10 @@ const medicationSchema = new mongoose.Schema({
 });
 
 const patientSchema = new mongoose.Schema({
-  patientHospitalId: { type: String, required: true, unique: true },
-  patientName: { type: String, required: true },
-  doctorName: { type: String, required: true },
-  hospitalName: { type: String, required: true },
+  patientHospitalId: { type: String, required: true },
+  patientName: { type: String },
+  doctorName: { type: String },
+  hospitalName: { type: String },
   SOPDocument: { type: String },
   patientDocument: { type: String },
   medications: [medicationSchema],
